7a53fd75c1fbdf0b5c9c73143f54e2475e687693
Ruben <ruben.astudillo@gmail.com>
2022-10-18 07:18:36 +0200

    Creo la clase receta y el componente de listado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListadoComponent } from './components/listado/listado.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListadoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/modelo/receta.ts
@@ -0,0 +1,10 @@
+export class Receta {
+    id: number;
+    nombre: string;
+    categoria: string;
+    etiquetas: string;
+    ingredientes: string;
+    instrucciones: string;
+    notas: string;
+    fechaAlta: string;
+}
